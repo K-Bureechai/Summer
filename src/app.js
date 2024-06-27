997aeac1b560865812c1cfc061b87286c7af9767
May <k.bureechai@gmail.com>
2024-06-27 21:44:22 +0700

    adjust CSS and injected weather icon's image

--- src/app.js
+++ src/app.js
@@ -7,6 +7,8 @@ function refreshWeather(response) {
   let windSpeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let icon = document.querySelector("#icon");
+  
   
 
   cityElement.innerHTML = response.data.city;
@@ -15,6 +17,8 @@ function refreshWeather(response) {
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
+  icon.innerHTML = `<img src="${response.data.condition.icon_url}"
+                    class="weather-icon"/>`;
 }
 
 function formatDate(date) {
